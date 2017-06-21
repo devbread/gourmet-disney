5306b8c689cf91e5f1741db8d32503727748520f
devbread <flores@cabeza.cl>
2017-06-21 13:20:32 -0400

    con loader

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 jQuery(document).ready(function(){
 	//WOW	
+
+
+$("body").queryLoader2({
+    backgroundColor: "#1F1F1F",
+    percentage: true,
+    deepSearch: true,
+    barHeight: 0,
+    completeAnimation: "fade",
+    onLoadComplete: function() {
+        new WOW().init();
+    }
+});
+
+
+  
 wow = new WOW(
       {
       boxClass:     'wow',      // default
